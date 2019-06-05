d138b89123e3e62cfd5bb3af99d934ae023ab1ad
max1303 <max1303@rambler.ru>
2019-06-05 20:53:34 +0300

    first

---
+++ course/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Курсы по программированию</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style_1200.css">
+	<link rel="stylesheet" type="text/css" href="css/style_1000.css">
+	<link rel="stylesheet" type="text/css" href="css/style_300.css">
+
+	<meta name='viewport' content='width=device-width,initial-scale=1'/>
+	<meta content='true' name='HandheldFriendly'/>
+	<meta content='width' name='MobileOptimized'/>
+	<meta content='yes' name='apple-mobile-web-app-capable'/>
+<!-- 	<link rel="shortcut icon" href="img/favicon.png" type="image/png"> -->
+</head>
+
+<body>
+
+<div id = "cont">
+
+	<header>
+
+	</header>	
+
+	<section id = "s1">	
+
+		<span class = "title">
+			<font style = "color: #099">Онлайн</font> курсы по <br> програм&shy;мированию
+		</span> 	<br><br><br><br>
+
+		<div>
+			<span class = "desc">
+				Данный курс предназначен для взрослых и детей от 14 лет. 
+				Курс состоит из теории и практических упражнений, а также домашних заданий.
+			</span>	  <br><br><br>
+
+			<img id = "man" src = "img/man.png">
+
+		</div>		<br><br><br>		
+
+	</section>
+
+	<div class = "button">	Записаться	</div>		<br><br><br>
+
+	<section id = "s2">
+		<span class = "title"> Программа курса</span>		<br><br><br><br>
+
+		<div>
+			<span class = "desc">
+				Курс длится приблизительно 6 мес. За это время вы получите навыки по следующим языкам:
+			</span>	
+		</div>		<br><br><br>
+
+		<div class = "lang"> 1. HTML - язык разметки.  </div> <br>
+
+		<div class = "lang"> 2. CSS - таблица стилей.  </div>  <br>
+
+		<div class = "lang"> 3. PHP - объектно-ориентированный язык программирования.  </div>	<br>
+
+		<div class = "lang"> 4. MySQL - система управления базой данных.  </div>	<br>
+
+		<div class = "lang"> 5. JavaScript - объектно-ориентированный язык программирования.  </div>	<br>				
+	</section>
+
+
+
+test test test
+
+
+
+
+
+	<footer>
+		
+		Создание сайта: Кибермозг.рф
+	</footer>
+
+</div>    <!-- cont -->
+
+<script src = "code.js"></script>
+
+</body>
+</html>
